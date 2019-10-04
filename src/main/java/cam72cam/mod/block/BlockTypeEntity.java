c4c54d9e39730570426af3494fbc3b513957909b
Christian Mesh <christianmesh1@gmail.com>
2019-10-04 18:58:39 -0400

    Hack togther LibBlockAttributes for blocks

--- src/main/java/cam72cam/mod/block/BlockTypeEntity.java
+++ src/main/java/cam72cam/mod/block/BlockTypeEntity.java
@@ -1,9 +1,25 @@
 package cam72cam.mod.block;
 
+import alexiil.mc.lib.attributes.*;
+import alexiil.mc.lib.attributes.fluid.FixedFluidInv;
+import alexiil.mc.lib.attributes.fluid.FluidExtractable;
+import alexiil.mc.lib.attributes.fluid.FluidInsertable;
+import alexiil.mc.lib.attributes.fluid.FluidInvTankChangeListener;
+import alexiil.mc.lib.attributes.fluid.volume.FluidKey;
+import alexiil.mc.lib.attributes.fluid.volume.FluidKeys;
+import alexiil.mc.lib.attributes.fluid.volume.FluidVolume;
+import alexiil.mc.lib.attributes.item.FixedItemInv;
+import alexiil.mc.lib.attributes.item.InvMarkDirtyListener;
+import alexiil.mc.lib.attributes.item.compat.FixedInventoryVanillaWrapper;
 import cam72cam.mod.block.tile.TileEntity;
 import cam72cam.mod.block.tile.TileEntityTickable;
 import cam72cam.mod.block.tile.TileEntityTickableTrack;
+import cam72cam.mod.energy.IEnergy;
 import cam72cam.mod.entity.Player;
+import cam72cam.mod.fluid.Fluid;
+import cam72cam.mod.fluid.FluidStack;
+import cam72cam.mod.fluid.ITank;
+import cam72cam.mod.item.IInventory;
 import cam72cam.mod.item.ItemStack;
 import cam72cam.mod.math.Vec3d;
 import cam72cam.mod.math.Vec3i;
@@ -12,20 +28,27 @@ import cam72cam.mod.util.Facing;
 import cam72cam.mod.util.Hand;
 import cam72cam.mod.util.ITrack;
 import cam72cam.mod.world.World;
+import io.github.cottonmc.energy.api.DefaultEnergyTypes;
+import io.github.cottonmc.energy.api.EnergyAttribute;
+import io.github.cottonmc.energy.api.EnergyType;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockEntityProvider;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.entity.EntityContext;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.Inventory;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.BlockView;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
 public abstract class BlockTypeEntity extends BlockType {
     protected final Identifier id;
-    private final BlockEntityType<TileEntity> teType;
+    private final BlockEntityType<? extends TileEntity> teType;
 
     public BlockTypeEntity(BlockSettings settings, Supplier<BlockEntity> constructData) {
         super(settings);
@@ -125,7 +148,7 @@ public abstract class BlockTypeEntity extends BlockType {
         return 1;
     }
 
-    protected class BlockTypeInternal extends BlockInternal implements BlockEntityProvider {
+    protected class BlockTypeInternal extends BlockInternal implements BlockEntityProvider, AttributeProvider {
         @Override
         public final boolean hasBlockEntity() {
             return true;
@@ -161,6 +184,168 @@ public abstract class BlockTypeEntity extends BlockType {
             }
 
             return Block.createCuboidShape(0, 0, 0, 1, Math.max(BlockTypeEntity.this.getHeight(World.get(entity.getWorld()), new Vec3i(pos)), 0.25)+0.1, 1);
+        }
+
+        @Override
+        public void addAllAttributes(net.minecraft.world.World world, BlockPos pos, BlockState state, AttributeList<?> to) {
+            net.minecraft.block.entity.BlockEntity be = world.getBlockEntity(pos);
+            if (be instanceof TileEntity) {
+                TileEntity te = (TileEntity) be;
+                ITank tank = te.getTank(Facing.UP);
+                if (tank != null) {
+                    to.offer(new FixedFluidInv() {
+
+                        @Override
+                        public int getTankCount() {
+                            return 1;
+                        }
+
+                        @Override
+                        public FluidVolume getInvFluid(int tankId) {
+                            return tank.getContents().internal;
+                        }
+
+                        @Override
+                        public int getMaxAmount(int tankId) {
+                            return tank.getCapacity();
+                        }
+
+                        @Override
+                        public boolean isFluidValidForTank(int tankId, FluidKey fluid) {
+                            return tank.allows(Fluid.getFluid(fluid));
+                        }
+
+                        @Override
+                        public ListenerToken addListener(FluidInvTankChangeListener listener, ListenerRemovalToken removalToken) {
+                            return null;
+                        }
+
+                        @Override
+                        public boolean setInvFluid(int tankId, FluidVolume to, Simulation simulation) {
+                            return false;
+                        }
+
+                        @Override
+                        public FluidInsertable getInsertable() {
+                            return (fluid, simulation) -> {
+                                int inserted = tank.fill(new FluidStack(fluid), simulation.isSimulate());
+                                return FluidVolume.create(fluid.getFluidKey(), fluid.getAmount() - inserted);
+                            };
+                        }
+
+                        @Override
+                        public FluidExtractable getExtractable() {
+                            return (filter, amount, simulation) -> {
+                                if (filter.matches(tank.getContents().internal.getFluidKey())) {
+                                    return tank.drain(new FluidStack(tank.getContents().getFluid(), amount), simulation.isSimulate()).internal;
+                                }
+                                return FluidVolume.create(FluidKeys.EMPTY, 0);
+                            };
+                        }
+                    });
+                }
+
+                IInventory inv = ((TileEntity) be).getInventory(Facing.UP);
+                if (inv != null) {
+                    to.offer(new FixedInventoryVanillaWrapper(new Inventory() {
+                        @Override
+                        public int getInvSize() {
+                            return inv.getSlotCount();
+                        }
+
+                        @Override
+                        public boolean isInvEmpty() {
+                            for (int i = 0; i < inv.getSlotCount(); i++) {
+                                if (!inv.get(i).isEmpty()) {
+                                    return false;
+                                }
+                            }
+                            return true;
+                        }
+
+                        @Override
+                        public net.minecraft.item.ItemStack getInvStack(int var1) {
+                            return inv.get(var1).internal;
+                        }
+
+                        @Override
+                        public net.minecraft.item.ItemStack takeInvStack(int var1, int var2) {
+                            return inv.extract(var1, var2, false).internal;
+                        }
+
+                        @Override
+                        public net.minecraft.item.ItemStack removeInvStack(int var1) {
+                            return inv.extract(var1, inv.get(var1).getCount(), false).internal;
+                        }
+
+                        @Override
+                        public void setInvStack(int var1, net.minecraft.item.ItemStack var2) {
+                            inv.set(var1, new ItemStack(var2));
+                        }
+
+                        @Override
+                        public void markDirty() {
+                            // NOP
+                        }
+
+                        @Override
+                        public boolean canPlayerUseInv(PlayerEntity var1) {
+                            return true;
+                        }
+
+                        @Override
+                        public void clear() {
+                            for (int i = 0; i< inv.getSlotCount(); i++) {
+                                removeInvStack(i);
+                            }
+                        }
+                    }));
+                }
+
+                IEnergy energy = ((TileEntity) be).getEnergy(Facing.UP);
+                if (energy != null) {
+                    to.offer(new EnergyAttribute() {
+
+                        @Override
+                        public int getMaxEnergy() {
+                            return energy.getMaxEnergyStored();
+                        }
+
+                        @Override
+                        public int getCurrentEnergy() {
+                            return energy.getEnergyStored();
+                        }
+
+                        @Override
+                        public boolean canInsertEnergy() {
+                            return true;
+                        }
+
+                        @Nonnull
+                        @Override
+                        public int insertEnergy(EnergyType energyType, int i, Simulation simulation) {
+                            return energy.receiveEnergy(energyType.convertFrom(DefaultEnergyTypes.MEDIUM_VOLTAGE, i), simulation.isSimulate());
+                        }
+
+                        @Override
+                        public boolean canExtractEnergy() {
+                            return true;
+                        }
+
+                        @Nonnull
+                        @Override
+                        public int extractEnergy(EnergyType energyType, int i, Simulation simulation) {
+                            return energyType.convertFrom(DefaultEnergyTypes.MEDIUM_VOLTAGE, energy.extractEnergy(energyType.convertFrom(DefaultEnergyTypes.MEDIUM_VOLTAGE, i), simulation.isSimulate()));
+                        }
+
+                        @Nonnull
+                        @Override
+                        public EnergyType getPreferredType() {
+                            return DefaultEnergyTypes.MEDIUM_VOLTAGE;
+                        }
+                    });
+                }
+            }
         }
     }
 
