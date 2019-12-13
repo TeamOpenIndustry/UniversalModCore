42db3d045b953561b4c7898bf70fc770170256b2
Christian Mesh <christianmesh1@gmail.com>
2019-12-12 20:44:31 -0500

    Fix mixins

--- src/main/java/cam72cam/mod/event/mixin/WorldMixin.java
+++ src/main/java/cam72cam/mod/event/mixin/WorldMixin.java
@@ -2,11 +2,18 @@ package cam72cam.mod.event.mixin;
 
 import cam72cam.mod.event.CommonEvents;
 import net.fabricmc.fabric.api.event.world.WorldTickCallback;
+import net.minecraft.util.profiler.Profiler;
 import net.minecraft.world.World;
+import net.minecraft.world.chunk.ChunkManager;
+import net.minecraft.world.dimension.Dimension;
+import net.minecraft.world.dimension.DimensionType;
+import net.minecraft.world.level.LevelProperties;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.function.BiFunction;
 
 @Mixin(World.class)
 public class WorldMixin {
@@ -15,7 +22,7 @@ public class WorldMixin {
     }
 
     @Inject(method = "<init>", at = @At("RETURN"))
-    public void init(CallbackInfo info) {
+    public void init(LevelProperties levelProperties_1, DimensionType dimensionType_1, BiFunction<World, Dimension, ChunkManager> biFunction_1, Profiler profiler_1, boolean boolean_1, CallbackInfo info) {
         CommonEvents.World.LOAD.execute(x -> x.accept((World)(Object)this));
     }
 }
--- src/main/java/cam72cam/mod/event/mixin/WorldRendererMixin.java
+++ src/main/java/cam72cam/mod/event/mixin/WorldRendererMixin.java
@@ -1,9 +1,14 @@
 package cam72cam.mod.event.mixin;
 
 import cam72cam.mod.event.ClientEvents;
+import net.minecraft.block.BlockState;
 import net.minecraft.client.render.Camera;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.WorldRenderer;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.hit.HitResult;
+import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,13 +17,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(WorldRenderer.class)
 public class WorldRendererMixin {
 
-    @Inject(method = "drawHighlightedBlockOutline", at = @At("HEAD"))
-    public void drawHighlightedBlockOutline(Camera camera_1, HitResult hitResult_1, int int_1, CallbackInfo info) {
-        if (hitResult_1.getType() == HitResult.Type.BLOCK) {
+    @Inject(method = "drawBlockOutline", at = @At("HEAD"))
+    public void drawHighlightedBlockOutline(MatrixStack matrixStack_1, VertexConsumer vertexConsumer_1, Entity entity_1, double double_1, double double_2, double double_3, BlockPos blockPos_1, BlockState blockState_1, CallbackInfo info) {
             // TODO MinecraftClient mc = MinecraftClient.getInstance();
             //mc.isPaused() ? mc.pausedTickDelta : mc.renderTickCounter.tickDelta
             ClientEvents.RENDER_MOUSEOVER.execute(x -> x.accept(0f));
-        }
-
     }
 }
