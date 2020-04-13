1858e2ca310c5af6686330057dce6ca0133ce882
Christian Mesh <christianmesh1@gmail.com>
2020-04-13 08:03:38 -0400

    Cleanup Mod.java

--- src/main/resources/template/src/main/java/PACKAGEPATH/Mod.java
+++ src/main/resources/template/src/main/java/PACKAGEPATH/Mod.java
@@ -8,14 +8,7 @@ public class Mod {
 
     static {
         try {
-            Class<ModCore.Mod> cls = (Class<ModCore.Mod>) Class.forName("#PACKAGE#.#CLASS#");
-            ModCore.register(() -> {
-                try {
-                    return cls.newInstance();
-                } catch (InstantiationException | IllegalAccessException e) {
-                    throw new RuntimeException("Could not construct mod " + MODID, e);
-                }
-            });
+            ModCore.register(#PACKAGE#.#CLASS#::new);
         } catch (Exception e) {
             throw new RuntimeException("Could not load mod " + MODID, e);
         }
