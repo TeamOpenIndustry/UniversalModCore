7fed049a6d3570fcfd67ae9fb1cc4fa912f48cdd
Christian Mesh <christianmesh1@gmail.com>
2020-10-12 18:12:40 -0400

    Add some comments to BlockInfo and ChunkManager

--- src/main/java/cam72cam/mod/world/BlockInfo.java
+++ src/main/java/cam72cam/mod/world/BlockInfo.java
@@ -1,6 +1,5 @@
 package cam72cam.mod.world;
 
-import cam72cam.mod.serialization.SerializationException;
 import cam72cam.mod.serialization.TagCompound;
 import cam72cam.mod.serialization.TagField;
 import cam72cam.mod.serialization.TagMapped;
@@ -8,6 +7,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTUtil;
 
+/** Represents a Block in-world and all of it's data (not counting TE) */
 @TagMapped(BlockInfo.TagMapper.class)
 public class BlockInfo {
     final IBlockState internal;
@@ -18,7 +18,7 @@ public class BlockInfo {
 
     public static class TagMapper implements cam72cam.mod.serialization.TagMapper<BlockInfo> {
         @Override
-        public TagAccessor<BlockInfo> apply(Class<BlockInfo> type, String fieldName, TagField tag) throws SerializationException {
+        public TagAccessor<BlockInfo> apply(Class<BlockInfo> type, String fieldName, TagField tag) {
             return new TagAccessor<>(
                     (d, o) -> {
                         if (o == null) {
