7cea3950a4056acf4fc25f67c7f2ec707c013cfe
Christian Mesh <christianmesh1@gmail.com>
2020-10-20 20:14:34 -0400

    Add DegreeFunc stuff

---
+++ src/test/java/cam72cam/mod/util/DegreeFuncsTest.java
@@ -0,0 +1,45 @@
+package cam72cam.mod.util;
+
+import junit.framework.TestCase;
+
+public class DegreeFuncsTest extends TestCase {
+
+    public void testNormalize() {
+        float[] cases = new float[]{
+                0, 0,
+                180, 180,
+                -180, 180,
+                360, 0,
+                -360, 0,
+                400, 40,
+                -400, 320,
+                360 * 10 + 30, 30,
+                -360 * 10 -30, 330,
+        };
+        for (int i = 0; i < cases.length; i+=2) {
+            assertEquals(cases[i+1], DegreeFuncs.normalize(cases[i]));
+        }
+    }
+
+    public void testDelta() {
+        float[] cases = new float[]{
+                0, 0, 0,
+                180, 180, 0,
+                -180, 180, 0,
+                360, 0, 0,
+                -360, 0, 0,
+                400, 40, 0,
+                -400, 320, 0,
+                360 * 10 + 30, 30, 0,
+                -360 * 10 -30, 330, 0,
+                10, 20, 10,
+                0, 179, 179,
+                0, 180, 180,
+                0, 181, 179,
+                -90, 90, 180,
+        };
+        for (int i = 0; i < cases.length; i+=3) {
+            assertEquals(cases[i+2], DegreeFuncs.delta(cases[i], cases[i+1]));
+        }
+    }
+}
