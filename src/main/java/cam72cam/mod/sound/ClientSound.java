bd1e94b5997750aa6d83856b3380be9e3e92ebd9
Christian Mesh <christianmesh1@gmail.com>
2020-12-02 17:24:36 -0500

    Refuse to play null ogg file

--- src/main/java/cam72cam/mod/sound/ClientSound.java
+++ src/main/java/cam72cam/mod/sound/ClientSound.java
@@ -41,6 +41,9 @@ class ClientSound implements ISound {
     }
 
     void init() {
+        if (oggLocation == null) {
+            return;
+        }
         id = MathHelper.getRandomUUID(ThreadLocalRandom.current()).toString();
         sndSystem.get().newSource(false, id, resource, oggLocation.toString(), repeats, 0f, 0f, 0f, AttenuationType.LINEAR.getTypeInt(), attenuationDistance);
     }
