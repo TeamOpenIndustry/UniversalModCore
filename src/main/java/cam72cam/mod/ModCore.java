2b81e740a3d166b0b8e28c130fff1d5e0df5e55c
Christian Mesh <christianmesh1@gmail.com>
2022-02-20 07:29:23 -0500

    Release 1.1.4

--- src/main/java/cam72cam/mod/ModCore.java
+++ src/main/java/cam72cam/mod/ModCore.java
@@ -37,7 +37,7 @@ import java.util.List;
 public class ModCore {
     public static final String MODID = "universalmodcore";
     public static final String NAME = "UniversalModCore";
-    public static final String VERSION = "1.1.3";
+    public static final String VERSION = "1.1.4";
     public static ModCore instance;
 
     private List<Mod> mods = new ArrayList<>();
