4cb787b9d171fdc578e0bad87d2af9d2a2e0cb27
Christian Mesh <christianmesh1@gmail.com>
2022-07-25 08:32:08 -0400

    Fix packet logging error

--- src/main/java/cam72cam/mod/net/Packet.java
+++ src/main/java/cam72cam/mod/net/Packet.java
@@ -115,7 +115,7 @@ public abstract class Packet {
     /** Broadcast to all players from server */
     public void sendToAll() {
         if (FMLEnvironment.dist == Dist.CLIENT) {
-            ModCore.warn("Warning, trying to send %s client side!");
+            ModCore.warn("Warning, trying to send %s client side!", this);
             return;
         }
         net.send(PacketDistributor.ALL.noArg(), new Message(this));
