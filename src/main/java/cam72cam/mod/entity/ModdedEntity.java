d5b228dc674393981b835330103ef6e8f706a1e8
poizzy <69535636+poizzy@users.noreply.github.com>
2025-06-23 21:25:54 +0200

    [1.12.2] Fixed missing passenger position sync. More hacky than anything else but it works

--- src/main/java/cam72cam/mod/entity/ModdedEntity.java
+++ src/main/java/cam72cam/mod/entity/ModdedEntity.java
@@ -365,6 +365,10 @@ public class ModdedEntity extends Entity implements IEntityAdditionalSpawnData {
             passenger.internal.rotationYaw = passenger.internal.rotationYaw + delta;
 
             seat.shouldSit = iRidable.shouldRiderSit(passenger);
+
+            if (self.getWorld().isServer) {
+                new PassengerPositionsPacket(this).sendToObserving(self);
+            }
         }
     }
 
