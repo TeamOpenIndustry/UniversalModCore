0591b6badab95482557fb7b257514eab6aa90f0a
Goldenfield192 <1437356849@qq.com>
2025-08-08 09:11:57 +0800

    feat: add back 1.12.2-style check box

--- src/main/java/cam72cam/mod/gui/screen/CheckBox.java
+++ src/main/java/cam72cam/mod/gui/screen/CheckBox.java
@@ -2,29 +2,28 @@ package cam72cam.mod.gui.screen;
 
 
 import cam72cam.mod.entity.Player;
-import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.client.gui.widget.button.CheckboxButton;
+import net.minecraft.util.text.TranslationTextComponent;
 
 /** Basic checkbox */
 public abstract class CheckBox extends Button {
     public CheckBox(IScreenBuilder builder, int x, int y, String text, boolean enabled) {
-        super(builder, x-25, y, 200, 20, (enabled ? "X" : "█") + " " + text);
+//        super(builder, x, y, 200, 20, (enabled ? "X" : "█") + " " + text);
+        super(builder, new CheckboxButton(builder.getWidth() / 2 + x, builder.getHeight() / 4 + y, 100, 20,
+                                          new TranslationTextComponent(text), enabled));
     }
 
     public boolean isChecked() {
-        return button.getMessage().getString().contains("X");
+        return ((CheckboxButton)button).selected();
     }
 
     @Override
     protected void onClickInternal(Player.Hand hand) {
-        this.setChecked(!this.isChecked());
+        this.setChecked();
         super.onClickInternal(hand);
     }
 
-    public void setChecked(boolean val) {
-        if (val) {
-            button.setMessage(new StringTextComponent(button.getMessage().getString().replace("█", "X")));
-        } else {
-            button.setMessage(new StringTextComponent(button.getMessage().getString().replace("X", "█")));
-        }
+    public void setChecked() {
+        ((CheckboxButton)button).onPress();
     }
 }
