5992fea5bffff398b181575581dc1ff9b4d23236
Goldenfield192 <1437356849@qq.com>
2025-09-06 10:30:47 +0800

    feat: introduce mixin and bump gradle version

---
+++ src/main/java/cam72cam/mod/UMCMixinLoadingPlugin.java
@@ -0,0 +1,43 @@
+package cam72cam.mod;
+
+
+import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.spongepowered.asm.launch.MixinBootstrap;
+import org.spongepowered.asm.mixin.Mixins;
+
+import javax.annotation.Nullable;
+import java.util.Map;
+
+@IFMLLoadingPlugin.MCVersion("1.12.2")
+public class UMCMixinLoadingPlugin implements IFMLLoadingPlugin {
+    public UMCMixinLoadingPlugin() {
+        MixinBootstrap.init();
+        Mixins.addConfiguration("mixins.universalmodcore.json");
+    }
+
+    @Override
+    public String[] getASMTransformerClass() {
+        return new String[0];
+    }
+
+    @Override
+    public String getModContainerClass() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getSetupClass() {
+        return null;
+    }
+
+    @Override
+    public void injectData(Map<String, Object> data) {
+
+    }
+
+    @Override
+    public String getAccessTransformerClass() {
+        return null;
+    }
+}
