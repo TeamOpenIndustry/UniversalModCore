29cacb21d88aa17490f06609426062a3e94043e5
Goldenfield192 <1437356849@qq.com>
2025-09-07 16:20:58 +0800

    fix: build runs

--- src/main/java/cam72cam/mod/UMCMixinLoadingPlugin.java
+++ src/main/java/cam72cam/mod/UMCMixinLoadingPlugin.java
@@ -1,10 +1,16 @@
 package cam72cam.mod;
 
+import net.minecraftforge.fml.relauncher.CoreModManager;
 import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
+import org.apache.logging.log4j.LogManager;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
 import javax.annotation.Nullable;
+import java.io.File;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.security.CodeSource;
 import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.12.2")
@@ -12,6 +18,23 @@ public class UMCMixinLoadingPlugin implements IFMLLoadingPlugin {
     public UMCMixinLoadingPlugin() {
         MixinBootstrap.init();
         Mixins.addConfiguration("mixins.universalmodcore.json");
+
+        //https://github.com/ReplayMod/ReplayMod/blob/ec50efec104c8345a3a4f20fd44cfb3608cd81bb/src/main/java/com/replaymod/core/LoadingPlugin.java
+        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
+        if (codeSource != null) {
+            URL location = codeSource.getLocation();
+            try {
+                File file = new File(location.toURI());
+                if (file.isFile()) {
+                    CoreModManager.getIgnoredMods().remove(file.getName());
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            }
+        } else {
+            LogManager.getLogger().warn("No CodeSource, if this is not a development environment we might run into problems!");
+            LogManager.getLogger().warn(this.getClass().getProtectionDomain());
+        }
     }
 
     @Override
