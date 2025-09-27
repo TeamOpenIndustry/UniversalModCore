a97352f11af236a6e7d4caf121d7070ef74f60b6
Goldenfield192 <119470535+Goldenfield192@users.noreply.github.com>
2025-09-27 20:14:39 +0800

    [Feat] Optimize OBJFace related performance (#1)
    * feat: optimize performance
    
    * fix: wrong offset
    
    * refactor
    
    * docs

---
+++ src/main/java/cam72cam/mod/model/obj/FaceAccessor.java
@@ -0,0 +1,216 @@
+package cam72cam.mod.model.obj;
+
+import cam72cam.mod.math.Vec3d;
+
+import javax.annotation.Nonnull;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * A {@link VertexBuffer} API wrapper, make accessing geometry complete separate from backend.
+ */
+public class FaceAccessor implements Iterable<FaceAccessor> {
+    // Cache VBOs to avoid costly re-fetching, even if they are removed it won't cause too much waste(1-2 MB)
+    private static final HashMap<String, VertexBuffer> vbos = new HashMap<>();
+    private final OBJModel model;
+
+    /**
+     * Current face's vertex accessors, assuming all faces are triangles.
+     */
+    public VertexAccessor v0;
+    public VertexAccessor v1;
+    public VertexAccessor v2;
+
+    private final VertexBuffer vbo;
+    private final boolean canSplit;
+    private int currentFaceIndex;
+    private final int startFace;
+    private final int endFace;
+
+    /**
+     * Internal, use OBJModel.getFaceAccessor
+     */
+    protected FaceAccessor(OBJModel model) {
+        this(model, 0, Integer.MAX_VALUE);
+    }
+
+    protected FaceAccessor(OBJModel model, int startFace, int endFace) {
+        this(model, startFace, endFace, true);
+    }
+
+    protected FaceAccessor(OBJModel model, int startFace, int endFace, boolean canSplit) {
+        this.model = model;
+        this.vbo = vbos.computeIfAbsent(model.hash, i -> model.vbo.buffer.get());
+        int faceCount = this.vbo.data.length / this.vbo.stride / this.vbo.vertsPerFace;
+        v0 = new VertexAccessor(0);
+        v1 = new VertexAccessor(1);
+        v2 = new VertexAccessor(2);
+        if (endFace < startFace) {
+            throw new IllegalStateException();
+        }
+        this.startFace = Math.max(0, startFace);
+        this.endFace = Math.min(faceCount, endFace);
+        this.currentFaceIndex = this.startFace;
+        this.canSplit = canSplit;
+    }
+
+    /**
+     * Get another {@link FaceAccessor} of certain group.
+     * @param groupName Desired group's name
+     * @return A {@link FaceAccessor} of given group, or {@code null} if group not present or this FaceAccessor is already grouped
+     */
+    public FaceAccessor getSubByGroup(String groupName) {
+        if (!model.groups.containsKey(groupName) || !canSplit) {
+            return null;
+        }
+        OBJGroup group = model.groups.get(groupName);
+        return new FaceAccessor(model, group.faceStart, group.faceStop + 1, false);
+    }
+
+    /**
+     * Convert current accessing face into an {@link OBJFace}.
+     * @return OBJFace of current face
+     */
+    public OBJFace asOBJFace() {
+        OBJFace face = new OBJFace();
+        face.vertices.add(v0.posAsVec3d());
+        face.vertices.add(v1.posAsVec3d());
+        face.vertices.add(v2.posAsVec3d());
+        face.uv.add(v0.uvAsVec2f());
+        face.uv.add(v1.uvAsVec2f());
+        face.uv.add(v2.uvAsVec2f());
+        if (vbo.hasNormals) {
+            face.normal = v0.normAsVec3d();
+        } else {
+            Vec3d v0 = face.vertices.get(0);
+            Vec3d v1 = face.vertices.get(1);
+            Vec3d v2 = face.vertices.get(2);
+            face.normal = v1.subtract(v0).crossProduct(v2.subtract(v0)).normalize();
+        }
+        return face;
+    }
+
+    /**
+     * An iterator designed specifically for for-each loop, use {@code asOBJFace} to create snapshot if you want to store it externally.
+     * @return Iterator of self, only recommend to use in for-each
+     */
+    @Override
+    @Nonnull
+    public Iterator<FaceAccessor> iterator() {
+        return new FaceIterator();
+    }
+
+    private class FaceIterator implements Iterator<FaceAccessor> {
+        private int iteratorIndex;
+
+        public FaceIterator() {
+            this.iteratorIndex = startFace;
+            currentFaceIndex = startFace;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iteratorIndex < endFace;
+        }
+
+        @Override
+        public FaceAccessor next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            currentFaceIndex = iteratorIndex++;
+            return FaceAccessor.this;
+        }
+
+        @Override
+        public void remove() {
+            Iterator.super.remove();
+        }
+    }
+
+    /**
+     * Vertex data accessor. Provides typed access to vertex attributes.
+     * <br>
+     * Use snapshot methods (posAsVec3d, etc.) to store data externally.
+     */
+    public class VertexAccessor {
+        public final int vertOffset;
+
+        protected VertexAccessor(int vertOffset) {
+            this.vertOffset = vertOffset;
+        }
+
+        public Vec3d posAsVec3d() {
+            return new Vec3d(x(), y(), z());
+        }
+
+        public Vec2f uvAsVec2f() {
+            return new Vec2f(u(), v());
+        }
+
+        public Vec3d normAsVec3d() {
+            if (!vbo.hasNormals) {
+                return Vec3d.ZERO;
+            }
+            return new Vec3d(nx(), ny(), nz());
+        }
+
+        public float x() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride];
+        }
+
+        public float y() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 1];
+        }
+
+        public float z() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 2];
+        }
+
+        public float u() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 3];
+        }
+
+        public float v() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 4];
+        }
+
+        public float r() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 5];
+        }
+
+        public float g() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 6];
+        }
+
+        public float b() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 7];
+        }
+
+        public float a() {
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 8];
+        }
+
+        public float nx() {
+            if (!vbo.hasNormals) {
+                return 0;
+            }
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 9];
+        }
+
+        public float ny() {
+            if (!vbo.hasNormals) {
+                return 0;
+            }
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 10];
+        }
+
+        public float nz() {
+            if (!vbo.hasNormals) {
+                return 0;
+            }
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 11];
+        }
+    }
+}
