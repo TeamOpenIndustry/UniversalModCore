1838d4cfb52e1d28baff16fd84cb0acc01993460
Goldenfield192 <1437356849@qq.com>
2025-11-01 19:01:17 +0800

    feat: add MixinExtras

---
+++ src/main/java/cam72cam/mod/UMCMixinLoader.java
@@ -0,0 +1,31 @@
+package cam72cam.mod;
+
+import com.llamalad7.mixinextras.MixinExtrasBootstrap;
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+//Used for initializing MixinExtras
+public class UMCMixinLoader implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String s) {
+        MixinExtrasBootstrap.init();
+    }
+
+    @Override
+    public String getRefMapperConfig() {return "";}
+    @Override
+    public boolean shouldApplyMixin(String s, String s1) {return true;}
+    @Override
+    public void acceptTargets(Set<String> set, Set<String> set1) {}
+    @Override
+    public List<String> getMixins() {return Collections.emptyList();}
+    @Override
+    public void preApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {}
+    @Override
+    public void postApply(String s, ClassNode classNode, String s1, IMixinInfo iMixinInfo) {}
+}
