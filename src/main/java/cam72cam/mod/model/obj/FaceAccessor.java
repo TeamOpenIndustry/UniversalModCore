35f5f5dd4c4a3c1108d79c446c1af1859ffd6aef
Goldenfield192 <1437356849@qq.com>
2025-11-16 11:34:03 +0800

    ref: remove magic number in FaceAccessor

--- src/main/java/cam72cam/mod/model/obj/FaceAccessor.java
+++ src/main/java/cam72cam/mod/model/obj/FaceAccessor.java
@@ -166,60 +166,60 @@ public class FaceAccessor implements Iterable<FaceAccessor> {
         }
 
         public float x() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.vertexOffset];
         }
 
         public float y() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 1];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.vertexOffset + 1];
         }
 
         public float z() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 2];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.vertexOffset + 2];
         }
 
         public float u() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 3];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.textureOffset];
         }
 
         public float v() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 4];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.textureOffset + 1];
         }
 
         public float r() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 5];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.colorOffset];
         }
 
         public float g() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 6];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.colorOffset + 1];
         }
 
         public float b() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 7];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.colorOffset + 2];
         }
 
         public float a() {
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 8];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.colorOffset + 3];
         }
 
         public float nx() {
             if (!vbo.hasNormals) {
                 return 0;
             }
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 9];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.normalOffset];
         }
 
         public float ny() {
             if (!vbo.hasNormals) {
                 return 0;
             }
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 10];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.normalOffset + 1];
         }
 
         public float nz() {
             if (!vbo.hasNormals) {
                 return 0;
             }
-            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + 11];
+            return vbo.data[(currentFaceIndex * 3 + vertOffset) * vbo.stride + vbo.normalOffset + 2];
         }
     }
 }
