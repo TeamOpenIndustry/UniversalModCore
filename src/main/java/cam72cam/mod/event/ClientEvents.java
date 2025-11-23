61f655a4bfb2ea8c26d41de0c5d94dcc98a1c861
Goldenfield192 <1437356849@qq.com>
2025-11-23 21:16:55 +0800

    ref: re-add RenderLevelLastEvent

--- src/main/java/cam72cam/mod/event/ClientEvents.java
+++ src/main/java/cam72cam/mod/event/ClientEvents.java
@@ -100,7 +100,7 @@ public class ClientEvents {
     public static final Event<Consumer<DrawSelectionEvent.HighlightBlock>> RENDER_MOUSEOVER = new Event<>();
     public static final Event<Consumer<SoundLoadEvent>> SOUND_LOAD = new Event<>();
     public static final Event<Runnable> RELOAD = new Event<>();
-//    public static final Event<Consumer<RenderLevelLastEvent>> OPTIFINE_SUCKS = new Event<>();
+    public static final Event<Consumer<RenderLevelLastEvent>> OPTIFINE_SUCKS = new Event<>();
 
     @Mod.EventBusSubscriber(modid = ModCore.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
     public static class ClientEventBusForge {
@@ -233,10 +233,10 @@ public class ClientEvents {
             SOUND_LOAD.execute(x -> x.accept(event));
         }
 
-//        @SubscribeEvent
-//        public static void optifineSucksEvent(RenderLevelLastEvent event) {
-//            OPTIFINE_SUCKS.execute(x -> x.accept(event));
-//        }
+        @SubscribeEvent
+        public static void optifineSucksEvent(RenderLevelLastEvent event) {
+            OPTIFINE_SUCKS.execute(x -> x.accept(event));
+        }
 
         static boolean hasHacked = false;
         @SubscribeEvent
