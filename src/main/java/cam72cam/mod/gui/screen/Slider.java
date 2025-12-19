8777047cc8bd0551bf6d8bb3faa81ca33d80e2a9
Goldenfield192 <1437356849@qq.com>
2025-12-19 21:46:57 +0800

    feat: add width/height for Slider

--- src/main/java/cam72cam/mod/gui/screen/Slider.java
+++ src/main/java/cam72cam/mod/gui/screen/Slider.java
@@ -8,12 +8,17 @@ import java.util.function.Consumer;
 /** Standard slider */
 public class Slider extends Button {
     public Slider(IScreenBuilder builder, int x, int y, String text, double min, double max, double start, boolean doublePrecision, Consumer<Slider> handler) {
+        this(builder, x, y, 150, 20, text, min, max, start, doublePrecision, handler);
+    }
+
+    public Slider(IScreenBuilder builder, int x, int y, int width, int height, String text, double min, double max, double start, boolean doublePrecision, Consumer<Slider> handler) {
         super(builder,
-              new GuiSlider(-1, builder.getWidth() / 2 + x, builder.getHeight() / 4 + y, text, min, max, start, null),
+              new GuiSlider(-1, builder.getWidth() / 2 + x, builder.getHeight() / 4 + y, width, height,
+                            text, "", min, max, start, doublePrecision, true, null),
               ((hand, button1) -> handler.accept((Slider) button1)));
-        ((GuiSlider) this.button).showDecimal = doublePrecision;
         ((GuiSlider) this.button).parent = slider -> Slider.this.onSlider();
     }
+
 
     @Deprecated
     public Slider(IScreenBuilder builder, int x, int y, String text, double min, double max, double start, boolean doublePrecision) {
