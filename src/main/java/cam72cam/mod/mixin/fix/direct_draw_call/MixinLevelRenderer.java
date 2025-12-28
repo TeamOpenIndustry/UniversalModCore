6e7a3a9775c8f68876dbf90cc538d0d96c442aa7
Goldenfield192 <1437356849@qq.com>
2025-12-28 14:29:45 +0800

    resolve conflicts

--- src/main/java/cam72cam/mod/mixin/fix/direct_draw_call/MixinLevelRenderer.java
+++ src/main/java/cam72cam/mod/mixin/fix/direct_draw_call/MixinLevelRenderer.java
@@ -4,12 +4,12 @@ import cam72cam.mod.render.ShaderHelper;
 import cam72cam.mod.render.opengl.RenderContext;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.math.Matrix4f;
 import net.minecraft.client.Camera;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GameRenderer;
 import net.minecraft.client.renderer.LevelRenderer;
 import net.minecraft.client.renderer.LightTexture;
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -21,7 +21,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinLevelRenderer {
     //For DirectDraw call
     //Render our ones delayed for better occlusion
-    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/LevelRenderer;renderDebug(Lnet/minecraft/client/Camera;)V"))
+    @Inject(method = "renderLevel", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/vertex/PoseStack;popPose()V", ordinal = 3))
     public void renderDeferred(PoseStack stack, float partialTicks, long p_228426_3_, boolean p_228426_5_,
                                Camera info, GameRenderer gameRenderer, LightTexture light,
                                Matrix4f matrix, CallbackInfo ci) {
