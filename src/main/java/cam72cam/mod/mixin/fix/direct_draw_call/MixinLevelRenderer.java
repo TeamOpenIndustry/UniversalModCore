478395940c37e20104c3c0e8060c8d206107fa95
Goldenfield192 <1437356849@qq.com>
2026-01-04 19:53:37 +0800

    post-merge fix

--- src/main/java/cam72cam/mod/mixin/fix/direct_draw_call/MixinLevelRenderer.java
+++ src/main/java/cam72cam/mod/mixin/fix/direct_draw_call/MixinLevelRenderer.java
@@ -1,5 +1,6 @@
 package cam72cam.mod.mixin.fix.direct_draw_call;
 
+import cam72cam.mod.render.ShaderHelper;
 import cam72cam.mod.render.opengl.RenderContext;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -38,7 +39,7 @@ public class MixinLevelRenderer {
     //Enable depthMask if we have something special to render
     @Redirect(method = "renderSnowAndRain", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;depthMask(Z)V", ordinal = 0))
     public void allowDepthMask(boolean p_69459_) {
-        if (RenderContext.hasDeferred()) {
+        if (RenderContext.hasDeferred() && !ShaderHelper.isIrisShaderEnabled()) {
             RenderSystem.depthMask(true);
         } else {
             RenderSystem.depthMask(p_69459_);
@@ -47,7 +48,7 @@ public class MixinLevelRenderer {
 
     @Redirect(method = "renderWorldBorder", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;depthMask(Z)V", ordinal = 0))
     public void allowBoarderDepthMask(boolean p_69459_) {
-        if (RenderContext.hasDeferred()) {
+        if (RenderContext.hasDeferred() && !ShaderHelper.isIrisShaderEnabled()) {
             RenderSystem.depthMask(true);
         } else {
             RenderSystem.depthMask(p_69459_);
--- src/main/java/cam72cam/mod/render/ShaderHelper.java
+++ src/main/java/cam72cam/mod/render/ShaderHelper.java
@@ -5,10 +5,20 @@ import org.lwjgl.opengl.ARBShaderObjects;
 /** For Internal use only */
 public class ShaderHelper {
     public static boolean isShaderPackEnabled() {
-        return isOptiFineEnabled();
+        return isIrisShaderEnabled() || isOptiFineEnabled();
     }
 
     public static boolean isOptiFineEnabled() {
         return OptiFine.isLoaded() && ARBShaderObjects.glGetHandleARB(ARBShaderObjects.GL_PROGRAM_OBJECT_ARB) != 0;
+    }
+
+    public static boolean isIrisShaderEnabled() {
+        try {
+            Class<?> clazz = Class.forName("net.irisshaders.iris.api.v0.IrisApi");
+            Object instance = clazz.getMethod("getInstance").invoke(null);
+            return (boolean) instance.getClass().getMethod("isShaderPackInUse").invoke(instance);
+        } catch (Throwable e) {
+            return false;
+        }
     }
 }
