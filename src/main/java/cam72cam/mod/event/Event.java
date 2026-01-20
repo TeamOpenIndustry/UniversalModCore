2c363a76d82ff8dada1fb71b8aaba2b6323f178f
Goldenfield192 <1437356849@qq.com>
2026-01-20 17:39:32 +0800

    feat: add flushable events support

--- src/main/java/cam72cam/mod/event/Event.java
+++ src/main/java/cam72cam/mod/event/Event.java
@@ -8,31 +8,46 @@ import java.util.function.Function;
 public class Event<T> {
     private final Set<Runnable> pre = new LinkedHashSet<>();
     private final Set<T> callbacks = new LinkedHashSet<>();
+    private final Set<T> flushableCallbacks = new LinkedHashSet<>();
     private final Set<Runnable> post = new LinkedHashSet<>();
 
     public void pre(Runnable callback) {
         pre.add(callback);
     }
+
+    //If this event is fired only 1 time per game launch or should be handled indifferently
     public void subscribe(T callback) {
         callbacks.add(callback);
     }
+
+    //If this event is fired multiple times per game launch and should be handled separately
+    public void subscribeFlushable(T callback) {
+        subscribe(callback);
+        flushableCallbacks.add(callback);
+    }
+
     public void post(Runnable callback) {
         post.add(callback);
     }
+
     void execute(Consumer<T> handler) {
         pre.forEach(Runnable::run);
         callbacks.forEach(handler);
+        callbacks.removeAll(flushableCallbacks);
+
         post.forEach(Runnable::run);
     }
 
     boolean executeCancellable(Function<T, Boolean> handler) {
         pre.forEach(Runnable::run);
-        for (T callback : callbacks) {
+        for (T callback : new LinkedHashSet<>(callbacks)) {
             if (!handler.apply(callback)) {
+                callbacks.removeAll(flushableCallbacks);
                 return false;
             }
         }
         post.forEach(Runnable::run);
+        callbacks.removeAll(flushableCallbacks);
         return true;
     }
 }
