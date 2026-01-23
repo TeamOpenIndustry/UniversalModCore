7c87aa5a9d378b953a845254d07412a6ce0ccb8e
Goldenfield192 <1437356849@qq.com>
2026-01-23 21:05:46 +0800

    ref: rename&reconstruct API

--- src/main/java/cam72cam/mod/event/Event.java
+++ src/main/java/cam72cam/mod/event/Event.java
@@ -6,24 +6,16 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 
 public class Event<T> {
-    private final Set<Runnable> pre = new LinkedHashSet<>();
-    private final Set<T> callbacks = new LinkedHashSet<>();
-    private final Set<T> flushableCallbacks = new LinkedHashSet<>();
-    private final Set<Runnable> post = new LinkedHashSet<>();
+    final Set<Runnable> pre = new LinkedHashSet<>();
+    final Set<T> callbacks = new LinkedHashSet<>();
+    final Set<Runnable> post = new LinkedHashSet<>();
 
     public void pre(Runnable callback) {
         pre.add(callback);
     }
 
-    //If this event is fired only 1 time per game launch or should be handled indifferently
     public void subscribe(T callback) {
         callbacks.add(callback);
-    }
-
-    //If this event is fired multiple times per game launch and should be handled separately
-    public void subscribeFlushable(T callback) {
-        subscribe(callback);
-        flushableCallbacks.add(callback);
     }
 
     public void post(Runnable callback) {
@@ -33,21 +25,35 @@ public class Event<T> {
     void execute(Consumer<T> handler) {
         pre.forEach(Runnable::run);
         callbacks.forEach(handler);
-        callbacks.removeAll(flushableCallbacks);
 
         post.forEach(Runnable::run);
     }
 
     boolean executeCancellable(Function<T, Boolean> handler) {
         pre.forEach(Runnable::run);
-        for (T callback : new LinkedHashSet<>(callbacks)) {
+        for (T callback : callbacks) {
             if (!handler.apply(callback)) {
-                callbacks.removeAll(flushableCallbacks);
                 return false;
             }
         }
         post.forEach(Runnable::run);
-        callbacks.removeAll(flushableCallbacks);
         return true;
+    }
+
+    /**
+     * For those events fired multiple times and should be handled respectively
+     */
+    static class TransientEvent<T> extends Event<T> {
+        @Override
+        void execute(Consumer<T> handler) {
+            super.execute(handler);
+            callbacks.clear();
+        }
+
+        boolean executeCancellable(Function<T, Boolean> handler) {
+            boolean result = super.executeCancellable(handler);
+            callbacks.clear();
+            return result;
+        }
     }
 }
