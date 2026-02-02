f9dc69168dcc5d3fbc4909091953a5508220d64f
Goldenfield192 <1437356849@qq.com>
2026-02-02 22:08:59 +0800

    fix: incorrect event type

--- src/main/java/cam72cam/mod/event/ClientEvents.java
+++ src/main/java/cam72cam/mod/event/ClientEvents.java
@@ -144,7 +144,7 @@ public class ClientEvents {
         }
         @SubscribeEvent
         public static void onGuiDrag(ScreenEvent.MouseDragged.Pre event) {
-            onGuiMouse(event, (int) event.getMouseX(), (int) event.getMouseY(), event.getMouseButton(), MouseAction.RELEASE);
+            onGuiMouse(event, (int) event.getMouseX(), (int) event.getMouseY(), event.getMouseButton(), MouseAction.MOVE);
         }
         @SubscribeEvent
         public static void onGuiRelease(ScreenEvent.MouseButtonReleased.Pre event) {
