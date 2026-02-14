663c49a3107f901521a1a6c430fe97f740bdbed1
Goldenfield192 <1437356849@qq.com>
2026-02-14 12:52:35 +0800

    ref: change injection point to non-OptiFine-sensitive

--- src/main/java/cam72cam/mod/mixin/feat/global_renderer/MixinRenderGlobal.java
+++ src/main/java/cam72cam/mod/mixin/feat/global_renderer/MixinRenderGlobal.java
@@ -14,7 +14,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(RenderGlobal.class)
 public class MixinRenderGlobal {
     @Inject(method = "renderEntities",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/tileentity/TileEntityRendererDispatcher;drawBatch(I)V"))
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/renderer/RenderGlobal;preRenderDamagedBlocks()V"))
     public void injectRenderGlobal(Entity renderViewEntity, ICamera camera, float partialTicks, CallbackInfo ci) {
         Vec3d pos = GlobalRender.getCameraPos(partialTicks);
         RenderState state = new RenderState().translate(-pos.x, -pos.y, -pos.z);
