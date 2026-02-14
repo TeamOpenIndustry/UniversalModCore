b6098d71c11a08e9b1d7f32e3ab00075e5a8d0b1
Goldenfield192 <1437356849@qq.com>
2026-02-14 16:30:15 +0800

    ref: RenderState.stage can no longer be null

--- src/main/java/cam72cam/mod/render/opengl/DirectDraw.java
+++ src/main/java/cam72cam/mod/render/opengl/DirectDraw.java
@@ -39,7 +39,7 @@ public class DirectDraw {
             }
             RenderSystem.setShader(() -> shader);
         };
-        if (state.stage != RenderContext.Stage.ENTITY) {
+        if (state.getStage() != RenderContext.Stage.ENTITY) {
             render.run();
         } else {
             RenderContext.addDeferred(render);
