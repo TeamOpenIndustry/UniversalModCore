8b5ccfa07607e45d880e1a7a6a78e135113f221c
Goldenfield192 <1437356849@qq.com>
2026-02-28 12:29:34 +0800

    fix: textfield input handling & expand input length to 256

--- src/main/java/cam72cam/mod/gui/screen/ScreenBuilder.java
+++ src/main/java/cam72cam/mod/gui/screen/ScreenBuilder.java
@@ -18,7 +18,8 @@ import java.util.function.Supplier;
 public class ScreenBuilder extends GuiScreen implements IScreenBuilder {
     private final IScreen screen;
     private final Map<GuiButton, Button> buttonMap = new HashMap<>();
-    private final List<GuiTextField> textFields = new ArrayList<>();
+    private final List<TextField> textFields = new ArrayList<>();
+    private TextField active = null;
     private final Supplier<Boolean> valid;
 
     public ScreenBuilder(IScreen screen, Supplier<Boolean> valid) {
@@ -83,7 +84,7 @@ public class ScreenBuilder extends GuiScreen implements IScreenBuilder {
 
     @Override
     public void addTextField(TextField textField) {
-        this.textFields.add(textField.textfield);
+        this.textFields.add(textField);
     }
 
     // GuiScreen
@@ -105,7 +106,7 @@ public class ScreenBuilder extends GuiScreen implements IScreenBuilder {
 
         screen.draw(this, new RenderState().stage(RenderContext.Stage.GUI));
 
-        textFields.forEach(GuiTextField::drawTextBox);
+        textFields.stream().map(t -> t.internal).forEach(GuiTextField::drawTextBox);
 
         // draw buttons
         super.drawScreen(mouseX, mouseY, partialTicks);
@@ -124,7 +125,7 @@ public class ScreenBuilder extends GuiScreen implements IScreenBuilder {
             close();
         }
 
-        if (this.textFields.stream().noneMatch(x -> x.textboxKeyTyped(typedChar, keyCode))) {
+        if (this.active != null && !active.internal.textboxKeyTyped(typedChar, keyCode)) {
             screen.onKeyType(this, Keyboard.KeyCode.of(keyCode));
         }
     }
@@ -141,9 +142,16 @@ public class ScreenBuilder extends GuiScreen implements IScreenBuilder {
             }
         }
 
-        if (this.textFields.stream().noneMatch(x -> x.mouseClicked(mouseX, mouseY, mouseButton))) {
-            screen.onMouseClick(mouseX, mouseY, hand);
+        for (TextField field : textFields) {
+            if (field.isVisible() && field.internal.mouseClicked(mouseX, mouseY, mouseButton)) {
+                active = field;
+                field.setFocused(true);
+                return;
+            }
+            field.setFocused(false);
         }
+
+        screen.onMouseClick(mouseX, mouseY, hand);
     }
 
     // Default overrides
--- src/main/java/cam72cam/mod/gui/screen/TextField.java
+++ src/main/java/cam72cam/mod/gui/screen/TextField.java
@@ -7,35 +7,36 @@ import java.util.function.Predicate;
 
 /** Base text field */
 public class TextField implements IWidget {
-    protected final GuiTextField textfield;
+    protected final GuiTextField internal;
 
     /** Standard constructor */
     public TextField(IScreenBuilder builder, int x, int y, int width, int height) {
         this(builder,
              //Offset x&y by 1 and minus width&height by 2 to let border be within the range specified
              new GuiTextField(-1, Minecraft.getMinecraft().fontRenderer, builder.getWidth() / 2 + x + 1, builder.getHeight() / 4 + y + 1, width - 2, height - 2));
+        internal.setMaxStringLength(256);
     }
 
     /** Internal, can be overridden to support custom GuiTextFields */
     protected TextField(IScreenBuilder builder, GuiTextField internal) {
-        this.textfield = internal;
+        this.internal = internal;
         builder.addTextField(this);
     }
 
     @Override
     public void setText(String s) {
-        textfield.setText(s);
+        internal.setText(s);
     }
 
     @Override
     public String getText() {
-        return textfield.getText();
+        return internal.getText();
     }
 
     @Override
     public void setVisible(boolean visible) {
-        textfield.setVisible(visible);
-        textfield.setEnabled(visible);
+        internal.setVisible(visible);
+        internal.setEnabled(visible);
     }
 
     @Deprecated
@@ -45,26 +46,26 @@ public class TextField implements IWidget {
 
     @Override
     public boolean isVisible() {
-        return textfield.getVisible();
+        return internal.getVisible();
     }
 
     @Override
     public void setEnabled(boolean enabled) {
-        textfield.setEnabled(enabled);
+        internal.setEnabled(enabled);
     }
 
     @Override
     public boolean isEnabled() {
-        return textfield.isEnabled;
+        return internal.isEnabled;
     }
 
     /** Validator that can block a string from being entered */
     public void setValidator(Predicate<String> filter) {
-        textfield.setValidator(filter::test);
+        internal.setValidator(filter::test);
     }
 
     /** Move cursor to this text field */
     public void setFocused(boolean b) {
-        textfield.setFocused(b);
+        internal.setFocused(b);
     }
 }
