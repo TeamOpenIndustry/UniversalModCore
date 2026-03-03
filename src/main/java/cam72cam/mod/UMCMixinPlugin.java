c7ce5da2abded80bbd6227aaa13e37405532cc88
Goldenfield192 <1437356849@qq.com>
2026-03-03 22:09:27 +0800

    rfg test on GH Actions

--- src/main/java/cam72cam/mod/UMCMixinPlugin.java
+++ src/main/java/cam72cam/mod/UMCMixinPlugin.java
@@ -1,36 +1,13 @@
 package cam72cam.mod;
 
-import cpw.mods.fml.relauncher.CoreModManager;
 import cpw.mods.fml.relauncher.IFMLLoadingPlugin;
-import cpw.mods.fml.common.FMLLog;
-import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
-import java.io.File;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.security.CodeSource;
 import java.util.Map;
 
 @IFMLLoadingPlugin.MCVersion("1.7.10")
 public class UMCMixinPlugin implements IFMLLoadingPlugin {
     public UMCMixinPlugin() {
-        MixinBootstrap.init();
-        Mixins.addConfiguration("mixins.feat.universalmodcore.json");
-        Mixins.addConfiguration("mixins.fix.universalmodcore.json");
-
-        CodeSource codeSource = this.getClass().getProtectionDomain().getCodeSource();
-        if (codeSource != null) {
-            URL location = codeSource.getLocation();
-            try {
-                File file = new File(location.toURI());
-                if (file.isFile()) {
-                    CoreModManager.getReparseableCoremods().add(file.getName());
-                }
-            } catch (URISyntaxException e) {
-                FMLLog.getLogger().warn(e);
-            }
-        }
     }
 
     @Override
@@ -50,7 +27,8 @@ public class UMCMixinPlugin implements IFMLLoadingPlugin {
 
     @Override
     public void injectData(Map<String, Object> data) {
-
+        Mixins.addConfiguration("mixins.feat.universalmodcore.json");
+        Mixins.addConfiguration("mixins.fix.universalmodcore.json");
     }
 
     @Override
