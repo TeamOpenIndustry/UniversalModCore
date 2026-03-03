3cfbe4d7f2a8e26d7a57f3c1dd9de5e28483e53a
Goldenfield192 <1437356849@qq.com>
2026-03-03 19:47:17 +0800

    chore: use MixinBooter for 1.10&1.11

--- src/main/java/cam72cam/mod/ModCore.java
+++ src/main/java/cam72cam/mod/ModCore.java
@@ -48,7 +48,7 @@ import java.util.Collections;
 import java.util.List;
 
 /** UMC Mod, do not touch... */
-@net.minecraftforge.fml.common.Mod(modid = ModCore.MODID, name = ModCore.NAME, version = ModCore.VERSION, acceptedMinecraftVersions = "[1.11,1.12)")
+@net.minecraftforge.fml.common.Mod(modid = ModCore.MODID, name = ModCore.NAME, version = ModCore.VERSION, acceptedMinecraftVersions = "[1.11,1.12)", dependencies = "required-after:mixinbooter@[10.7,)")
 public class ModCore {
     public static final String MODID = "universalmodcore";
     public static final String NAME = "UniversalModCore";
