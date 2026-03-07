a94ea1ad60e41e41e2ef965a2cb71100b766c8a0
Goldenfield192 <1437356849@qq.com>
2026-03-07 10:41:52 +0800

    fix: blocks destroyed by flowing fluid

--- src/main/java/cam72cam/mod/block/BlockType.java
+++ src/main/java/cam72cam/mod/block/BlockType.java
@@ -20,15 +20,17 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.LiquidBlockContainer;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.HitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
-import net.minecraftforge.registries.ForgeRegistries;
 
 /** A standard block with no attached entity */
 public abstract class BlockType {
@@ -148,7 +150,8 @@ public abstract class BlockType {
      * BlockInternal is an internal class that should only be extended when you need to implement
      * an interface.
      */
-    public class BlockInternal extends net.minecraft.world.level.block.Block {
+    public class BlockInternal extends net.minecraft.world.level.block.Block
+                               implements LiquidBlockContainer {
         public BlockInternal() {
             super(Block.Properties.of().mapColor(getMaterial().internal)
                     .sound(BlockType.this.getMaterial().soundType)
@@ -270,6 +273,17 @@ public abstract class BlockType {
         @Override
         public VoxelShape getOcclusionShape(BlockState state, BlockGetter worldIn, BlockPos pos) {
             return Shapes.empty();
+        }
+
+        //Blocks liquid movement for now
+        @Override
+        public boolean canPlaceLiquid(BlockGetter p_54766_, BlockPos p_54767_, BlockState p_54768_, Fluid p_54769_) {
+            return false;
+        }
+
+        @Override
+        public boolean placeLiquid(LevelAccessor p_54770_, BlockPos p_54771_, BlockState p_54772_, FluidState p_54773_) {
+            return false;
         }
     }
 }
