67dd77aca279964cf8610fe16a0ea016ee6a771c
Goldenfield192 <1437356849@qq.com>
2026-04-12 16:06:37 +0800

    fix: adapt to 1.16 datapack loading logic

--- src/main/java/cam72cam/mod/event/platform/LoadDatapackEvent.java
+++ src/main/java/cam72cam/mod/event/platform/LoadDatapackEvent.java
@@ -3,11 +3,12 @@ package cam72cam.mod.event.platform;
 import net.minecraft.resources.*;
 import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.fml.event.lifecycle.IModBusEvent;
 
 /**
  * Fired when datapacks are reloaded, useful when injecting your dynamic datapack implementations.
  */
-public class LoadDatapackEvent extends Event {
+public class LoadDatapackEvent extends Event implements IModBusEvent {
     private final ResourcePackList infos;
 
     public LoadDatapackEvent(ResourcePackList infos) {
--- src/main/java/cam72cam/mod/mixin/feat/data_registry/MixinMinecraftServer.java
+++ src/main/java/cam72cam/mod/mixin/feat/data_registry/MixinMinecraftServer.java
@@ -1,32 +1,23 @@
 package cam72cam.mod.mixin.feat.data_registry;
 
 import cam72cam.mod.event.platform.LoadDatapackEvent;
-import net.minecraft.resources.ResourcePackInfo;
 import net.minecraft.resources.ResourcePackList;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.world.storage.WorldInfo;
+import net.minecraft.util.datafix.codec.DatapackCodec;
 import net.minecraftforge.fml.ModLoader;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import java.io.File;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /**
  * Datapack loading callback
  */
 @Mixin(MinecraftServer.class)
 public class MixinMinecraftServer {
-    @Shadow
-    @Final
-    private ResourcePackList<ResourcePackInfo> resourcePacks;
-
-    @Inject(method = "loadDataPacks(Ljava/io/File;Lnet/minecraft/world/storage/WorldInfo;)V", at = @At("HEAD"))
-    public void callback(File p_195560_1_, WorldInfo p_195560_2_, CallbackInfo ci) {
-        LoadDatapackEvent event = new LoadDatapackEvent(this.resourcePacks);
+    @Inject(method = "configurePackRepository", at = @At("HEAD"))
+    private static void callback(ResourcePackList repo, DatapackCodec codec, boolean forceModData, CallbackInfoReturnable<DatapackCodec> cir) {
+        LoadDatapackEvent event = new LoadDatapackEvent(repo);
         ModLoader.get().postEvent(event);
     }
 }
