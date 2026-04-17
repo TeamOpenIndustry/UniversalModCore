c93a956b74283f505c8f73f9f0f045dec4ca05eb
Goldenfield192 <1437356849@qq.com>
2026-04-17 21:04:15 +0800

    ref: use WrapMethod to avoid refmap conflict

--- src/main/java/cam72cam/mod/mixin/feat/data_registry/MixinTagCollection.java
+++ src/main/java/cam72cam/mod/mixin/feat/data_registry/MixinTagCollection.java
@@ -2,6 +2,8 @@ package cam72cam.mod.mixin.feat.data_registry;
 
 import cam72cam.mod.event.platform.RegisterBlockTagEvent;
 import cam72cam.mod.event.platform.RegisterItemTagEvent;
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import net.minecraft.resources.IResourceManager;
 import net.minecraft.tags.Tag;
 import net.minecraft.tags.TagCollection;
@@ -10,11 +12,10 @@ import net.minecraftforge.fml.ModLoader;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
 
 /**
  * Used for posting <code>RegisterBlockTagEvent</code> and <code>RegisterItemTagEvent</code>
@@ -27,21 +28,25 @@ public class MixinTagCollection {
     @Final
     private String resourceLocationPrefix;
 
-    @Inject(method = "lambda$reload$3", at = @At("RETURN"))
-    public void postTagReload(IResourceManager manager, CallbackInfoReturnable<Map<ResourceLocation, Tag.Builder<?>>> cir) {
-        Map<ResourceLocation, Tag.Builder<?>> map = cir.getReturnValue();
-        switch (this.resourceLocationPrefix) {
-            //Change me when minecraft version changes
-            case "tags/blocks":
-                RegisterBlockTagEvent blockTagEvent = new RegisterBlockTagEvent(map);
-                ModLoader.get().postEvent(blockTagEvent);
-                return;
-            case "tags/items":
-                RegisterItemTagEvent itemTagEvent = new RegisterItemTagEvent(map);
-                ModLoader.get().postEvent(itemTagEvent);
-                return;
-            default:
-                //Waiting for more...
-        }
+    @WrapMethod(method = "reload")
+    public CompletableFuture<Map<ResourceLocation, Tag.Builder<?>>> register(IResourceManager p_242224_1_, Executor p_242224_2_, Operation<CompletableFuture<Map<ResourceLocation, Tag.Builder<?>>>> original) {
+        CompletableFuture<Map<ResourceLocation, Tag.Builder<?>>> target = original.call(p_242224_1_, p_242224_2_);
+        target.thenAccept(map -> {
+            switch (this.resourceLocationPrefix) {
+                //Change me when minecraft version changes
+                case "tags/blocks":
+                    RegisterBlockTagEvent blockTagEvent = new RegisterBlockTagEvent(map);
+                    ModLoader.get().postEvent(blockTagEvent);
+                    return;
+                case "tags/items":
+                    RegisterItemTagEvent itemTagEvent = new RegisterItemTagEvent(map);
+                    ModLoader.get().postEvent(itemTagEvent);
+                    return;
+                default:
+                    //Waiting for more...
+            }
+        });
+
+        return target;
     }
 }
