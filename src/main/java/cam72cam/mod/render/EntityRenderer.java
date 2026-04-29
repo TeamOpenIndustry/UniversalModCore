deefa1712b8826228cc5ae4ffef4d8440880234f
Goldenfield192 <1437356849@qq.com>
2026-04-29 21:04:31 +0800

    ref: make postRender transparent by default

--- src/main/java/cam72cam/mod/render/EntityRenderer.java
+++ src/main/java/cam72cam/mod/render/EntityRenderer.java
@@ -12,10 +12,8 @@ import cam72cam.mod.render.opengl.RenderContext;
 import cam72cam.mod.render.opengl.RenderState;
 import cam72cam.mod.world.World;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.renderer.ActiveRenderInfo;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
-import net.minecraft.client.renderer.RenderHelper;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.culling.ClippingHelperImpl;
@@ -25,10 +23,7 @@ import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.LightType;
 import net.minecraftforge.client.event.RenderWorldLastEvent;
-import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL13;
 
 import javax.annotation.Nullable;
 import java.util.HashMap;
@@ -163,10 +158,11 @@ public class EntityRenderer<T extends ModdedEntity> extends net.minecraft.client
 
         // State may be modified in render, before calling in to post-render
         renderers.get(self.getClass()).render(self, state.clone(), partialTicks);
-        // TODO
-        renderers.get(self.getClass()).postRender(self, state, partialTicks);
-
         RenderType.getCutout().clearRenderState();
+
+        RenderType.getTranslucent().setupRenderState();
+        renderers.get(self.getClass()).postRender(self, state, partialTicks);
+        RenderType.getTranslucent().clearRenderState();
     }
 
     @Nullable
