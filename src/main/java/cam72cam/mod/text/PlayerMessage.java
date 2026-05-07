951282983d92f3b5af01d19a8cc6a2836c7136c5
Goldenfield192 <1437356849@qq.com>
2026-05-07 19:20:05 +0800

    fix: strict message args

--- src/main/java/cam72cam/mod/text/PlayerMessage.java
+++ src/main/java/cam72cam/mod/text/PlayerMessage.java
@@ -3,6 +3,8 @@ package cam72cam.mod.text;
 import net.minecraft.network.chat.Component;
 import net.neoforged.neoforge.common.CommonHooks;
 
+import java.util.Arrays;
+
 /** A message that can be sent to a player */
 public class PlayerMessage {
     public final Component internal;
@@ -18,7 +20,8 @@ public class PlayerMessage {
 
     /** Translated */
     public static PlayerMessage translate(String msg, Object... objects) {
-        return new PlayerMessage(Component.translatable(msg, objects));
+        String[] args = Arrays.stream(objects).map(Object::toString).toArray(String[]::new);
+        return new PlayerMessage(Component.translatable(msg, args));
     }
 
     /** URL Formatted (clickable) */
