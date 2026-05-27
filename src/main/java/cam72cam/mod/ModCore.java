006a96443e6d9770b6283c53eb2b6b8f2d97b74a
Goldenfield192 <1437356849@qq.com>
2026-05-27 18:43:29 +0800

    bump version to 1.2.4

--- src/main/java/cam72cam/mod/ModCore.java
+++ src/main/java/cam72cam/mod/ModCore.java
@@ -46,7 +46,7 @@ import java.util.List;
 public class ModCore {
     public static final String MODID = "universalmodcore";
     public static final String NAME = "UniversalModCore";
-    public static final String VERSION = "1.2.3";
+    public static final String VERSION = "1.2.4";
     public static ModCore instance;
 
     private List<Mod> mods = new ArrayList<>();
