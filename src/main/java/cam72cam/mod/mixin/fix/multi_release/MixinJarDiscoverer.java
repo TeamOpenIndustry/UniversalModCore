847e65ea3d3daeb9f44323c4982121e9e615bca7
Goldenfield192 <1437356849@qq.com>
2026-05-27 20:16:13 +0800

    fix MixinJarDiscoverer on 1.7

--- src/main/java/cam72cam/mod/mixin/fix/multi_release/MixinJarDiscoverer.java
+++ src/main/java/cam72cam/mod/mixin/fix/multi_release/MixinJarDiscoverer.java
@@ -3,7 +3,7 @@ package cam72cam.mod.mixin.fix.multi_release;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraftforge.fml.common.discovery.JarDiscoverer;
+import cpw.mods.fml.common.discovery.JarDiscoverer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
