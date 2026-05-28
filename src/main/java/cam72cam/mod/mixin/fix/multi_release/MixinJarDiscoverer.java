ae5dc352621747b9987744638ad4787c848db6ce
Goldenfield192 <1437356849@qq.com>
2026-05-28 18:44:51 +0800

    resolve 1.14 conflicts

--- src/main/java/cam72cam/mod/mixin/fix/multi_release/MixinJarDiscoverer.java
+++ src/main/java/cam72cam/mod/mixin/fix/multi_release/MixinJarDiscoverer.java
@@ -1,24 +1,21 @@
 package cam72cam.mod.mixin.fix.multi_release;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraftforge.fml.common.discovery.JarDiscoverer;
+import net.minecraftforge.fml.loading.moddiscovery.AbstractJarFileLocator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import java.util.regex.Matcher;
-import java.util.zip.ZipEntry;
+import java.nio.file.Path;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
 
-@Mixin(JarDiscoverer.class)
+@Mixin(AbstractJarFileLocator.class)
 public class MixinJarDiscoverer {
     //Skip Java9+ classes in Multi-Release as Forge can't process them
-    @WrapOperation(method = "findClassesASM", at = @At(value = "INVOKE", target = "Ljava/util/regex/Matcher;matches()Z"), remap = false, require = 0)
-    private boolean checkClass(Matcher instance, Operation<Boolean> original, @Local(name = "ze") ZipEntry ze) {
-        if (original.call(instance)) {
-            return !ze.getName().endsWith("module-info.class")
-                   && !ze.getName().startsWith("META-INF/versions/");
-        }
-        return false;
+    @Inject(method = "lambda$scanFile$1", at = @At(value = "INVOKE", target = "Ljava/util/stream/Stream;forEach(Ljava/util/function/Consumer;)V"), remap = false, require = 0)
+    private static void checkClass(Consumer<Path> pathConsumer, Path path, CallbackInfo ci, @Local(name = "files") Stream<Path> files) {
+        files.filter(p -> !p.toString().endsWith("module-info.class") && !p.toString().startsWith("META-INF/versions/"));
     }
 }
