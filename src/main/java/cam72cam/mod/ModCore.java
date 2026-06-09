4347fbf4b2900ffde3af9fa7c9e972041d8bcc7d
Goldenfield192 <1437356849@qq.com>
2026-06-09 22:26:07 +0800

    feat: add ModCore.semanticVersion

--- src/main/java/cam72cam/mod/ModCore.java
+++ src/main/java/cam72cam/mod/ModCore.java
@@ -349,6 +349,10 @@ public class ModCore {
         return 11202;
     }
 
+    public static String semanticVersion() {
+        return "1.12.2-forge";
+    }
+
     private static Boolean isDev = null;
 
     public static boolean isDevelopmentEnvironment() {
