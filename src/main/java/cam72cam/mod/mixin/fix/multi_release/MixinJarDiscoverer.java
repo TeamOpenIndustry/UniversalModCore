9f4aa6673dc6884513dacad73ddfab982161adf1
Goldenfield192 <1437356849@qq.com>
2026-06-15 16:09:37 +0800

    ref: use Redirect to avoid MixinBooter crash

--- src/main/java/cam72cam/mod/mixin/fix/multi_release/MixinJarDiscoverer.java
+++ src/main/java/cam72cam/mod/mixin/fix/multi_release/MixinJarDiscoverer.java
@@ -1,11 +1,10 @@
 package cam72cam.mod.mixin.fix.multi_release;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import net.minecraftforge.fml.common.discovery.JarDiscoverer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.regex.Matcher;
 import java.util.zip.ZipEntry;
@@ -13,9 +12,11 @@ import java.util.zip.ZipEntry;
 @Mixin(JarDiscoverer.class)
 public class MixinJarDiscoverer {
     //Skip Java9+ classes in Multi-Release as Forge can't process them
-    @WrapOperation(method = "discover", at = @At(value = "INVOKE", target = "Ljava/util/regex/Matcher;matches()Z"), remap = false, require = 0)
-    private boolean checkClass(Matcher instance, Operation<Boolean> original, @Local(name = "ze") ZipEntry ze) {
-        if (original.call(instance)) {
+//    @WrapOperation(method = "discover", at = @At(value = "INVOKE", target = "Ljava/util/regex/Matcher;matches()Z"), remap = false, require = 0)
+    //TODO Change back to @WrapOperation on 1.11/1.10 once MixinBooter is fixed
+    @Redirect(method = "discover", at = @At(value = "INVOKE", target = "Ljava/util/regex/Matcher;matches()Z"), remap = false)
+    private boolean checkClass(Matcher instance, @Local(name = "ze") ZipEntry ze) {
+        if (instance.matches()) {
             return !ze.getName().endsWith("module-info.class")
                    && !ze.getName().startsWith("META-INF/versions/");
         }
