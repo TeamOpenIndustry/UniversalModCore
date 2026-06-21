62985a3c4e08afb8ac2f68188ac5ef06d1984d1a
Angelina Michalek <ianthisawesomee@gmail.com>
2026-06-21 19:04:34 -0400

    Change back, I made a mistake

--- src/main/resources/template/src/main/java/PACKAGEPATH/Mod.java
+++ src/main/resources/template/src/main/java/PACKAGEPATH/Mod.java
@@ -1,38 +1,16 @@
 package #PACKAGE#;
 
 import cam72cam.mod.ModCore;
-import cam72cam.mod.ModEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 @net.minecraftforge.fml.common.Mod(Mod.MODID)
-public class Mod extends ModCore.Mod {
+public class Mod {
     public static final String MODID = "#ID#";
 
     static {
         try {
-            ModCore.register(new Mod());
+            ModCore.register(new #PACKAGE#.#CLASS#());
         } catch (Exception e) {
             throw new RuntimeException("Could not load mod " + MODID, e);
         }
-    }
-
-    @Override
-    public String modID() {
-        return MODID;
-    }
-
-    @Override
-    public void commonEvent(ModEvent modEvent) {
-
-    }
-
-    @Override
-    public void clientEvent(ModEvent modEvent) {
-
-    }
-
-    @Override
-    public void serverEvent(ModEvent modEvent) {
-
     }
 }
