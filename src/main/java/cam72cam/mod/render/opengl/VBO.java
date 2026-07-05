3fce891777063bfebe9a685f7ac18fd141eb67b5
Goldenfield192 <1437356849@qq.com>
2026-07-05 10:42:37 +0800

    ref: clarify shader location

--- src/main/java/cam72cam/mod/render/opengl/VBO.java
+++ src/main/java/cam72cam/mod/render/opengl/VBO.java
@@ -42,7 +42,7 @@ public class VBO {
         ClientEvents.REGISTER_SHADER.subscribe(event -> {
             try {
                 event.registerShader(new ShaderInstance(event.getResourceManager(),
-                                                        ResourceLocation.parse("umc_core"),
+                                                        ResourceLocation.withDefaultNamespace("umc_core"),
                                                         DefaultVertexFormat.NEW_ENTITY),
                                      instance -> RenderContext.UMC_CORE = instance);
             } catch (IOException e) {
