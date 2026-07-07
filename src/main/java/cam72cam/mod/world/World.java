73753982947fe587427067e1b925585efad499b5
Goldenfield192 <1437356849@qq.com>
2026-07-07 14:06:06 +0800

    ref: align behavior with 1.12

--- src/main/java/cam72cam/mod/world/World.java
+++ src/main/java/cam72cam/mod/world/World.java
@@ -446,7 +446,7 @@ public class World {
 
     /** Height of the ground for precipitation purposes at the given block */
     public Vec3i getPrecipitationHeight(Vec3i pos) {
-        return new Vec3i(internal.getHeight(Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, pos.internal()));
+        return new Vec3i(internal.getHeight(Heightmap.Type.MOTION_BLOCKING, pos.internal()));
     }
 
     /** Set the given pos to air */
