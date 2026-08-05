8b7ac4ae4682819c408898c25f71d1e4b986e834
Goldenfield192 <1437356849@qq.com>
2026-08-05 20:18:00 +0800

    ref: optimize OBJFace

--- src/main/java/cam72cam/mod/model/obj/OBJFace.java
+++ src/main/java/cam72cam/mod/model/obj/OBJFace.java
@@ -4,32 +4,38 @@ import cam72cam.mod.entity.boundingbox.IBoundingBox;
 import cam72cam.mod.math.Vec3d;
 
 public class OBJFace {
-    public Vertex vertex0;
-    public Vertex vertex1;
-    public Vertex vertex2;
+    public final Vertex vertex0;
+    public final Vertex vertex1;
+    public final Vertex vertex2;
 
-    public Vec3d normal;
+    public final Vec3d normal;
+    //TODO more accurate one
+    private IBoundingBox box;
+
+    public OBJFace(Vertex vertex0, Vertex vertex1, Vertex vertex2, Vec3d normal) {
+        this.vertex0 = vertex0;
+        this.vertex1 = vertex1;
+        this.vertex2 = vertex2;
+        this.normal = normal;
+    }
 
     public IBoundingBox getBoundingBox() {
-        Vec3d min = vertex0.pos.min(vertex1.pos.min(vertex2.pos));
-        Vec3d max = vertex0.pos.max(vertex1.pos.max(vertex2.pos));
-        return IBoundingBox.from(min, max);
+        if (box == null) {
+            //Uses AABB for now but we may want OBB or something more accurate in the future
+            Vec3d min = vertex0.pos.min(vertex1.pos.min(vertex2.pos));
+            Vec3d max = vertex0.pos.max(vertex1.pos.max(vertex2.pos));
+            box = IBoundingBox.from(min, max);
+        }
+        return box;
     }
 
     public OBJFace scale(double factor) {
-        OBJFace scaled = new OBJFace();
-
-        scaled.vertex0 = vertex0.scale(factor);
-        scaled.vertex1 = vertex1.scale(factor);
-        scaled.vertex2 = vertex2.scale(factor);
-
-        scaled.normal = new Vec3d(normal.internal());
-        return scaled;
+        return new OBJFace(vertex0.scale(factor), vertex1.scale(factor), vertex2.scale(factor), normal);
     }
 
     public static class Vertex {
-        public Vec3d pos;
-        public Vec2f uv;
+        public final Vec3d pos;
+        public final Vec2f uv;
 
         public Vertex(Vec3d vertex, Vec2f uv) {
             this.pos = vertex;
