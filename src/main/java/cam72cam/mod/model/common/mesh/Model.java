edd0aecbc5d3477296bde6164cf6315ea9a617f0
Goldenfield192 <1437356849@qq.com>
2026-08-13 18:16:40 +0800

    feat: add group related things to Model

--- src/main/java/cam72cam/mod/model/common/mesh/Model.java
+++ src/main/java/cam72cam/mod/model/common/mesh/Model.java
@@ -1,11 +1,11 @@
 package cam72cam.mod.model.common.mesh;
 
+import cam72cam.mod.math.Vec3d;
+import cam72cam.mod.render.common.ModelRenderer;
 import cam72cam.mod.render.obj.OBJTextureSheet;
 import cam72cam.mod.resource.Identifier;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Model {
     private final Identifier location;
@@ -38,16 +38,84 @@ public class Model {
         this.normal.putAll(norm);
     }
 
+    // ModelGroup helpers
+
+    public LinkedHashMap<String, ModelGroup> getGroups() {
+        return groups;
+    }
+
+    public Set<String> groups() {
+        return groups.keySet();
+    }
+
+    public Vec3d minOfGroups(Iterable<String> groupNames) {
+        Vec3d min = null;
+        for (String group : groupNames) {
+            Vec3d gmin = groups.get(group).min;
+            if (min == null) {
+                min = gmin;
+            } else {
+                min = min.min(gmin);
+            }
+        }
+        return min;
+    }
+
+    public Vec3d maxOfGroups(Iterable<String> groupNames) {
+        Vec3d max = null;
+        for (String group : groupNames) {
+            Vec3d gmax = groups.get(group).max;
+            if (max == null) {
+                max = gmax;
+            } else {
+                max = max.max(gmax);
+            }
+        }
+        return max;
+    }
+
+    public Vec3d centerOfGroups(Iterable<String> groupNames) {
+        Vec3d min = minOfGroups(groupNames);
+        Vec3d max = maxOfGroups(groupNames);
+        return new Vec3d((min.x + max.x) / 2, (min.y + max.y) / 2, (min.z + max.z) / 2);
+    }
+
+    public double lengthOfGroups(Iterable<String> groupNames) {
+        Vec3d min = minOfGroups(groupNames);
+        Vec3d max = maxOfGroups(groupNames);
+        return max.x - min.x;
+    }
+
+    public double heightOfGroups(Iterable<String> groupNames) {
+        Vec3d min = minOfGroups(groupNames);
+        Vec3d max = maxOfGroups(groupNames);
+        return max.y - min.y;
+    }
+
+    public double widthOfGroups(Iterable<String> groupNames) {
+        Vec3d min = minOfGroups(groupNames);
+        Vec3d max = maxOfGroups(groupNames);
+        return max.z - min.z;
+    }
+
+    /** WARNING This is a very slow function and should be used for debug only */
+    public List<Vec3d> points(ModelGroup group) {
+        List<Vec3d> points = new ArrayList<>();
+        for (int face = group.faceStart; face <= group.faceEnd; face++) {
+            for (int point = 0; point < 3; point++) {
+                int idx = (face * 3 + point) * layout.getStride() + layout.getOffset(VAOLayout.Usage.POSITION);
+                points.add(new Vec3d(vboData[idx], vboData[idx+1], vboData[idx+2]));
+            }
+        }
+        return points;
+    }
+
     public Identifier location() {
         return location;
     }
 
     public VAOLayout getLayout() {
         return layout;
-    }
-
-    public LinkedHashMap<String, ModelGroup> getGroups() {
-        return groups;
     }
 
     public float[] getVboData() {
@@ -64,5 +132,24 @@ public class Model {
 
     public Map<String, Map<Integer, OBJTextureSheet>> getNormals() {
         return normal;
+    }
+
+    public void free() {
+        for (Map<Integer, OBJTextureSheet> lodMap : texture.values()) {
+            for (OBJTextureSheet texture : lodMap.values()) {
+                texture.dealloc();
+            }
+        }
+        for (Map<Integer, OBJTextureSheet> lodMap : specular.values()) {
+            for (OBJTextureSheet texture : lodMap.values()) {
+                texture.dealloc();
+            }
+        }
+        for (Map<Integer, OBJTextureSheet> lodMap : normal.values()) {
+            for (OBJTextureSheet texture : lodMap.values()) {
+                texture.dealloc();
+            }
+        }
+        ModelRenderer.getRendererFor(this).free();
     }
 }
