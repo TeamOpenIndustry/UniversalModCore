4e9b7357cdc1a82a64b032d265b2869f3a3243a7
Goldenfield192 <1437356849@qq.com>
2026-08-15 16:45:20 +0800

    mark old API as deprecated

--- src/main/java/cam72cam/mod/model/obj/OBJModel.java
+++ src/main/java/cam72cam/mod/model/obj/OBJModel.java
@@ -23,6 +23,10 @@ import java.util.stream.Collectors;
 
 import static cam72cam.mod.model.common.util.ImageUtils.*;
 
+/**
+ * Use {@link cam72cam.mod.model.common.ModelLoader#load} and {@link cam72cam.mod.model.common.mesh.Model}
+ */
+@Deprecated
 public class OBJModel {
     public static final OBJTextureSheet defTex = new OBJTextureSheet(1, 1, () -> new ResourceCache.GenericByteBuffer(new int[] { 0x0000FF }), Integer.MAX_VALUE/2);
     public final OBJRender vbo;
